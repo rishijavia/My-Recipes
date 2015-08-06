123b19d5420d04aeddaeb6742b904ab2a34b379b
rishijavia <rishijavia@lclark.edu>
2015-08-06 12:10:20 +0000

    Edited application.js and precompiled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
